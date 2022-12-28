7365b333b643c81604fd9d8c7f3195a69695de33
sirasak <sirasak554@gmail.com>
2022-12-28 16:57:31 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+//     reset: true
+});
+function sendMail() {
+    // alert('test');
+    var params = {
+      subject_name: document.getElementById("subject").value,
+      from_name: document.getElementById("from_name").value,
+      email: document.getElementById("from_email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_600p2wl";
+    const templateID = "template_05q6no8";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          console.log(res);
+          console.log('Thank, for contact me');
+          alert('Thank, for contact me');
+          document.getElementById("subject").value = "";
+          document.getElementById("from_name").value = "";
+          document.getElementById("from_email").value = "";
+          document.getElementById("message").value = "";
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
